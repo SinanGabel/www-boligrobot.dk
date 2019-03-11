45195e5f764d325c5facc469b641941916e02b01
sinangabel <sinan.gabel@gmail.com>
2019-03-11 19:08:17 +0100

    updated build script to tar and upload to gs

--- script/build_www.sh
+++ script/build_www.sh
@@ -21,6 +21,12 @@ mv js/bolig.js js/tmp.js
 
 java -jar $ROOT'/git-private/risk-cloud-functions/common/script/compiler.jar' --compilation_level SIMPLE_OPTIMIZATIONS --js $TEMP'www/js/tmp.js' --js_output_file $TEMP'www/js/bolig.js'
 
-rm -fR js/tmp.js js/data.js
+rm -fR js/tmp.js
+
+cd $TEMP
+
+tar -cvzf boligrobot.tgz www/
+
+gsutil cp boligrobot.tgz gs://777misc
 
 
